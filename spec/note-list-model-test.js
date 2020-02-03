c9a674da9034c0f76ea204b65b8bdaf9217b6ac8
niki-01 <niki.manoledaki@gmail.com>
2020-02-03 16:15:09 +0000

    Refactored

--- spec/note-list-model-test.js
+++ spec/note-list-model-test.js
@@ -1,5 +1,8 @@
 function testArrayReturnsNotes() {
   var list = new List();
+  list.newNote("My favourite language is JavaScript")
+  list.newNote("Favourite drink: seltzer")
+  
   assert.isTrue(list.array[0] === 'My favourite language is JavaScript');
   assert.isTrue(list.array[1] === 'Favourite drink: seltzer');
 }
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -1,5 +1,10 @@
 'use strict';
 
 function List() {
-  this.array = ['My favourite language is JavaScript', 'Favourite drink: seltzer']
+  // this.array = ['My favourite language is JavaScript', 'Favourite drink: seltzer']
+  this.array = []
+}
+
+List.prototype.newNote = function(text) {
+  this.array.push(text)
 }
