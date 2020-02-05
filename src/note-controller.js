22c329c9b658b14f1ecc0a7d8f3505fc2930f838
ria patel <ria.amy.patel@gmail.com>
2020-02-05 15:11:31 +0000

    working on step 6

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,7 +1,20 @@
-var element = document.getElementById('app')
 
-function changeText(text) {
-  element.innerHTML = text
-}
+(function(exports){
+  function Controller() {
+    this.noteList = new List()
+  }
 
-changeText('meghan is cool')
+  Controller.prototype.changeText = function(text) {
+    var element = document.getElementById('app')
+    element.innerHTML = text
+  }
+
+  var controller = function() {
+    return new Controller()
+  }
+
+  // var controller = new Controller(new List())
+  exports.controller = controller
+})(this);
+
+// var x = new Controller
